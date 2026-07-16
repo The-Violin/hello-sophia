826d7a9ce3571cb7f88acfd5db01100d68b2530e
Israel Joshua Jennings <israel_electron_capture@proton.me>
2026-07-16 10:49:20 -0600

    Add existing Python scripts

---
+++ M_Py_3.py
@@ -0,0 +1,29 @@
+# <!-- <p> <h2> ALPHABETIZING AND SORTING LISTS </h2> <p> -->
+
+# July 14th_2026 A.D.
+# Page 143 of Chapter 3_Dummies John Simpson Aaei?
+
+# listname.sort()
+
+# Create list of strings.
+disciples =["Peter", "James", "Judas", "Matthew", "Paul"]
+
+# Create list of numbers.
+numbers = [14, 0, 56, -4, 99, 56, 11, 23]
+
+# Sort the names list.
+disciples.sort()
+
+# Sort the numbers list.
+numbers.sort()
+
+# Show the results.
+print(disciples)
+print(numbers)
+
+OUTPUT:
+
+['James', 'Judas', 'Matthew', 'Paul', 'Peter']
+[-4, 0, 11, 14, 23, 56, 56, 99]
+
+=== Code Execution Successful ===
